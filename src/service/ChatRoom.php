7cc353c92441af7c12d9944eeb865281355c5b3c
zhanghaibo <zhanghaibo@mia.com>
2016-06-20 19:50:35 +0800

    聊天室

--- src/service/ChatRoom.php
+++ src/service/ChatRoom.php
@@ -1,9 +1,30 @@
 <?php
 namespace mia\miagroup\Service;
 
+use \RongCloud\Api;
+use \F_Ice;
 use \FS_Service;
-use mia\miagroup\Util\RongCloud;
 
-class ChatRoot extends FS_Service{
+/**
+ * 融云聊天室
+ * @author user
+ *
+ */
+class ChatRoom extends FS_Service{
+    
+    public $api = null;
+    
+    public function __construct(){
+        
+        $this->api  = new Api(F_Ice::$ins->workApp->config->get('busconf.rongcloud')['appKey'],F_Ice::$ins->workApp->config->get('busconf.rongcloud')['appSecret']);   
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
