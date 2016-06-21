a993aa410c9538468d944868d675a463b9e909c2
zhanghaibo <zhanghaibo@mia.com>
2016-06-21 12:51:49 +0800

    添加聊天室token

--- src/service/ChatRoom.php
+++ src/service/ChatRoom.php
@@ -12,11 +12,24 @@ use \FS_Service;
  */
 class ChatRoom extends FS_Service{
     
+    //融云sdk
     public $api = null;
     
     public function __construct(){
-        
         $this->api  = new Api(F_Ice::$ins->workApp->config->get('busconf.rongcloud')['appKey'],F_Ice::$ins->workApp->config->get('busconf.rongcloud')['appSecret']);   
+    }
+    
+    /**
+     * 获取token
+     * @param unknown $userId
+     * @param unknown $name
+     * @param unknown $portraitUri
+     */
+    public function getToken($userId, $name, $portraitUri){
+        $ret = $this->api->getToken($userId, $name, $portraitUri);
+        var_dump($ret);exit;
+//         $ret = json_decode($ret);
+//         return $this->succ($ret);
     }
     
     
