ee5ed43b501228cebe013f48eda46307f76c2e3a
zhanghaibo <zhanghaibo@mia.com>
2016-06-22 15:07:59 +0800

    去掉调试

--- src/webroot/service.php
+++ src/webroot/service.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE);
 require_once __DIR__ . '/../../vendor/autoload.php';
 $runner = new \Ice\Frame\Runner\Service(__DIR__ . '/../conf/app.php');
 $runner->run();
