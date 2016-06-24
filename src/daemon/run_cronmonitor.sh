0abf42410bc14d27fc61c64bf9d93758b911042e
hanxiang <hanxiang@mia.com>
2016-06-24 16:55:17 +0800

    daemon

--- src/daemon/run_cronmonitor.sh
+++ src/daemon/run_cronmonitor.sh
@@ -2,5 +2,5 @@
 while [ true ];
 do
   sleep 1
-  /usr/local/php/bin/php /var/www/html/xigua_sns_dev_1.0/service/classes/daemon/cron/monitor.php
+  /usr/local/php/bin/php /data/www/groupservice/src/daemon/cli.php --class=common --action=cronmonitor
 done
