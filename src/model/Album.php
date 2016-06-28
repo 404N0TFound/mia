8d879716a3957557fe2187adf78df28285f02ef9
yulei <yulei1@mia.com>
2016-06-28 10:03:08 +0800

    .

--- src/model/Album.php
+++ src/model/Album.php
@@ -12,12 +12,15 @@ class Album {
     public $albumData = '';
     public $userGroupDoozerData = '';
     public $albumPermissionData = '';
+    public $h5_url = '';
+    
 
     public function __construct() {
         $this->albumArticleData = new AlbumArticleData();
         $this->albumData = new AlbumData();
         $this->userGroupDoozerData = new GroupDoozer();
         $this->albumPermissionData = new AlbumPermissionData();
+        $this->h5_url = 'www.mia.com/groupspe/show/%d/%d';
     }
 
     /**
@@ -67,7 +70,7 @@ class Album {
                     $res[$key]['album_info']['user_id'] = $AlbumInfo[$value['album_id']]['user_id'];
                     $res[$key]['album_info']['title'] = $AlbumInfo[$value['album_id']]['title'];
                 }
-                $res[$key]['h5_url'] = '';//待续
+                $res[$key]['h5_url'] = sprintf('www.mia.com/groupspe/show/%d/%d',$value['id'],$value['album_id']);;//待续
                 $res[$key]['view_num'] = $this->readNum($value['create_time']);
                 $res[$key]['content'] = strip_tags(mb_substr($value['content'],0,50,'utf-8')).'....';
                 unset($res[$key]['create_time']);
