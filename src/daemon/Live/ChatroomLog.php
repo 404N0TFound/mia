f8baf5b0cd98da73d44d111ccc7b6b7c6cf91926
zhanghaibo <zhanghaibo@mia.com>
2016-06-28 15:30:25 +0800

    修改文件

--- src/daemon/Live/ChatroomLog.php
+++ src/daemon/Live/ChatroomLog.php
@@ -11,7 +11,42 @@ class ChatroomLog extends \FD_Daemon{
         $rong_api = new RongCloudUtil();
         $url = $rong_api->messageHistory($date);
         if(!empty($url)){
+            $url_info = pathinfo($url);
+            $chatroom_log_path = \F_Ice::$ins->workApp->config->get('app.chatroom_log_path');
+            $file_name = 'chatroomlog_'.$date.$url_info['extension'];
+            $dir = $chatroom_log_path.$file_name;
+            $this->curlDownload($url, $dir);
+            
+            //处理压缩文件
+//             $zip = new ZipArchive;
+//             if ($zip->open($file_name) === TRUE) {
+//                 $zip->extractTo('foldername');
+//                 $zip->close();
+//                 //success
+//             } else {
+//                 //fail
+//             }
             
         }
+    }
+    
+    /**
+     * crul下载文件
+     * @param unknown $url
+     * @param unknown $dir
+     */
+    public function curlDownload($url, $dir) {
+        $ch = curl_init($url);
+        $fp = fopen($dir, "wb");
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $res=curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+        
+        if(!$res){
+            die(curl_error($ch));
+        }
+        return $res;
     }
 }
