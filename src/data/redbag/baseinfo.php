2d0fa316678385aab1782242356a9c5e3e83df53
hanxiang <hanxiang@mia.com>
2016-07-01 18:57:24 +0800

    红包bug修改

--- src/data/redbag/baseinfo.php
+++ src/data/redbag/baseinfo.php
@@ -5,12 +5,12 @@ use Ice;
 
 class Baseinfo extends \DB_Query {
 
-	protected $dbResource = 'miagroup';
+    protected $dbResource = 'miagroup';
 
-	protected $tableName = 'redbag_base_info';
+    protected $tableName = 'redbag_base_info';
 
-	protected $mapping = array();
-	
+    protected $mapping = array();
+
     /**
      * 根据红包Ids批量获取红包信息
      */
@@ -22,16 +22,13 @@ class Baseinfo extends \DB_Query {
         $where[] = array(':in', 'redbag_id', $ids);
         $where[] = array(':eq', 'status', 2);
         $data = $this->getRows($where);
-
+        
         $result = array();
-        if(!empty($data)){
-            foreach($data as $v){
+        if (!empty($data)) {
+            foreach ($data as $v) {
                 $result[$v['redbag_id']] = $v;
             }
         }
         return $result;
     }
-
-
-
 }
