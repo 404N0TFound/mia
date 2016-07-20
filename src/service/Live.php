3b5b0ed80b1c109388c57bc151fe924434037f8d
zhanghaibo <zhanghaibo@mia.com>
2016-07-20 16:28:17 +0800

    修改发送红包消息不显示用户昵称的的bug

--- src/service/Live.php
+++ src/service/Live.php
@@ -516,7 +516,7 @@ class Live extends \FS_Service {
         }
         //发送抢到红包的消息
         $userService = new User();
-        $userInfo = $userService->getUserInfoByUserId($userId);
+        $userInfo = $userService->getUserInfoByUserId($userId)['data'];
         if (!empty($userInfo)) {
             $content = NormalUtil::getMessageBody(0, \F_Ice::$ins->workApp->config->get('busconf.user.miaTuUid'), sprintf('恭喜%s抢到%s元红包', $userInfo['nickname'], $redbagNums['data']));
             $this->rongCloud->messageChatroomPublish(NormalUtil::getConfig('busconf.rongcloud.fromUserId'), $liveRoomInfo['chat_room_id'], NormalUtil::getConfig('busconf.rongcloud.objectName'), $content);
