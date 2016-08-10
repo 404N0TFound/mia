09b0c31552c647c476de7ac307cf7cda4d248224
hanxiang <hanxiang@mia.com>
2016-08-10 10:20:32 +0800

    修复日志bug

--- src/lib/Service.php
+++ src/lib/Service.php
@@ -5,6 +5,7 @@ class Service extends \FS_Service {
 
     private $startTime;
     private $endTime;
+    static private $logFlag;
     
     function __construct() {
         parent::__construct();
@@ -12,14 +13,17 @@ class Service extends \FS_Service {
     }
     
     function __destruct() {
-        $this->endTime = gettimeofday(true);
-        $respTime = number_format(($this->endTime - $this->startTime), 4);
-        \F_Ice::$ins->mainApp->logger_access->info(array(
-            //'current_uid'         => '',    //当前调用接口的用户ID
-            'code'                => $this->code,                //接口返回code
-            'resp_time'           => $respTime,                  //接口响应时间
-            'params'              => $this->params,              //接口参数
-            'ext_params'          => $this->ext_params,          //其他参数
-        ));
+        if (self::$logFlag === null) {
+            self::$logFlag = 1; //设置日志标记，防止一次请求记录重复日志
+            $this->endTime = gettimeofday(true);
+            $respTime = number_format(($this->endTime - $this->startTime), 4);
+            \F_Ice::$ins->mainApp->logger_access->info(array(
+                //'current_uid'         => '',    //当前调用接口的用户ID
+                'code'                => $this->code,                //接口返回code
+                'resp_time'           => $respTime,                  //接口响应时间
+                'params'              => $this->params,              //接口参数
+                'ext_params'          => $this->ext_params,          //其他参数
+            ));
+        }
     }
 }
