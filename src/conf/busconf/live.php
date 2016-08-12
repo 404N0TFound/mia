060fec7e36c70dcb24723c895f06b2a6ef22bfe0
hanxiang <hanxiang@mia.com>
2016-08-12 14:18:00 +0800

    修改直播分享文案

--- src/conf/busconf/live.php
+++ src/conf/busconf/live.php
@@ -6,7 +6,7 @@
 $liveShareInfo = array(
     'live_by_user'      => array(
         'title'      => '%s@你',
-        'desc'      => '天啦撸！%s正在蜜芽直播，更有红包等你来领！快上车→',
+        'desc'      => '天啦噜！%s正在蜜芽直播，更有红包等你来领！快上车→',
         'wap_url'  => 'http://m.mia.com/mialive/live?roomid=%d&liveid=%d',
     ),
     'live_by_anchor'      => array(
