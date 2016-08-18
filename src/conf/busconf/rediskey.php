37b5b30a759d4c65a4cfb3ababfc3d2cd1805eb0
hanxiang <hanxiang@mia.com>
2016-08-18 18:55:32 +0800

    线上脚本问题

--- src/conf/busconf/rediskey.php
+++ src/conf/busconf/rediskey.php
@@ -14,15 +14,15 @@ $liveKey = array(
         'key' => $liveServicePrefix . 'audience_online_num_%s',
         'expire_time' => 3600,
     ),
-    //以售卖的商品数
+    //已售卖的商品数
     'live_sale_num' => array(
         'key' => $liveServicePrefix . 'sale_num_%s',
         'expire_time' => 3600,
     ),
-    //以售卖的商品数
+    //直播推流状态
     'live_stream_status' => array(
         'key' => $liveServicePrefix . 'stream_status_%s',
-        'expire_time' => 60,
+        'expire_time' => 86400,
     ),
     //直播转换成视频-状态记录
     'live_to_video' => [ //使用String数据结构
