b4966be1c68e8bb66ca316b2062bfe139a691620
hanxiang <hanxiang@mia.com>
2016-09-20 11:41:17 +0800

    审核服务

---
+++ src/model/Audit.php
@@ -0,0 +1,11 @@
+<?php
+namespace mia\miagroup\Model;
+use \F_Ice;
+use mia\miagroup\Data\Album\AlbumArticle as AlbumArticleData;
+use mia\miagroup\Data\Album\Album as AlbumData;
+use mia\miagroup\Data\Album\AlbumPermission as AlbumPermissionData;
+use mia\miagroup\Data\User\GroupDoozer as GroupDoozer;
+
+class Audit {
+    
+}
--- src/service/Audit.php
+++ src/service/Audit.php
@@ -1,9 +1,88 @@
 <?php
 namespace mia\miagroup\Service;
 
+use mia\miagroup\model\Audit as AuditModel;
+use mia\miagroup\service\User as UserService;
+
 /**
  * 审核服务
  */
 class Audit extends \mia\miagroup\Lib\Service {
     
+    private $auditModel;
+    
+    public function __construct() {
+        $this->auditModel = AuditModel();
+    }
+    
+    /**
+     * 验证用户是否已屏蔽
+     */
+    public function checkUserIsShield($userId) {
+        
+    }
+    
+    /**
+     * 验证设备是否已屏蔽
+     */
+    public function checkIsShieldByDevice($deviceInfo) {
+        
+    }
+    
+    /**
+     * 验证是否为有效用户
+     */
+    public function checkIsValidUser($userId) {
+        //获取用户信息
+        //判断是否有验证手机、邮箱，是否有设置头像、密码
+    }
+    
+    /**
+     * 查看用户是否为白名单用户
+     */
+    public function checkIsWhiteUser($userId, $deviceToken) {
+        
+    }
+    
+    /**
+     * 屏蔽用户
+     */
+    public function shieldUser() {
+        
+    }
+    
+    /**
+     * 屏蔽帖子
+     */
+    public function shieldSubject() {
+        
+    }
+    
+    /**
+     * 屏蔽评论
+     */
+    public function shieldComment() {
+        
+    }
+    
+    /**
+     * 获取屏蔽原因
+     */
+    public function getShieldReason() {
+        
+    }
+    
+    /**
+     * 检查敏感词
+     */
+    public function checkSensitiveWords($textArray) {
+        //兼容单条
+        if (is_string($textArray)) {
+            
+            return;
+        } else {
+            
+            return;
+        }
+    }
 }
