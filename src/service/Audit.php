7d01624a7fa0fb97ba1c662f1556c7824404c592
hanxiang <hanxiang@mia.com>
2016-09-20 16:08:53 +0800

    审核服务

--- src/service/Audit.php
+++ src/service/Audit.php
@@ -1,7 +1,7 @@
 <?php
 namespace mia\miagroup\Service;
 
-use mia\miagroup\model\Audit as AuditModel;
+use mia\miagroup\Model\Audit as AuditModel;
 use mia\miagroup\service\User as UserService;
 
 /**
@@ -12,21 +12,23 @@ class Audit extends \mia\miagroup\Lib\Service {
     private $auditModel;
     
     public function __construct() {
-        $this->auditModel = AuditModel();
+        $this->auditModel = new AuditModel();
     }
     
     /**
      * 验证用户是否已屏蔽
      */
     public function checkUserIsShield($userId) {
-        
+        $isShield = $this->auditModel->checkIsShieldByUid($userId);
+        return $this->succ(array('is_shield' => $isShield));
     }
     
     /**
      * 验证设备是否已屏蔽
      */
     public function checkIsShieldByDevice($deviceInfo) {
-        
+        $isShield = $this->auditModel->getDeviceShieldByDeviceInfo($deviceInfo);
+        return $this->succ(array('is_shield' => $isShield));
     }
     
     /**
@@ -34,7 +36,14 @@ class Audit extends \mia\miagroup\Lib\Service {
      */
     public function checkIsValidUser($userId) {
         //获取用户信息
+        $userService = new \mia\miagroup\Service\User();
         //判断是否有验证手机、邮箱，是否有设置头像、密码
+        $userInfo = $userService->getUserInfoByUserId($userId)['data'];
+        $isValid = false;
+        if (!empty($userInfo['icon']) || !empty($userInfo['is_id_verified']) || !empty($userInfo['is_cell_verified'])) {
+            $isValid = true;
+        }
+        return $this->succ(array('is_valid' => $isValid));
     }
     
     /**
@@ -76,13 +85,28 @@ class Audit extends \mia\miagroup\Lib\Service {
      * 检查敏感词
      */
     public function checkSensitiveWords($textArray) {
-        //兼容单条
-        if (is_string($textArray)) {
-            
-            return;
-        } else {
-            
-            return;
+        //获取敏感词
+        $sensitiveWord = $this->auditModel->getAllSensitiveWord();
+        $sensitiveWord = implode('|', $sensitiveWord);
+        //解除敏感词匹配个数限制
+        ini_set('pcre.backtrack_limit', -1);
+        
+        $matchList = array();
+        if (is_string($textArray)) { //兼容单条
+            preg_match_all("/".$sensitiveWord."/i", $textArray, $match);
+            if (isset($match[0]) && !empty($match[0])) {
+                $matchList = $match[0];
+            }
+        } else if (is_array($textArray)) {
+            foreach ($textArray as $text) {
+                $key = md5($text);
+                preg_match_all("/".$sensitiveWord."/i", $text, $match);
+                if (isset($match[0]) && !empty($match[0])) {
+                    $matchList[$key] = $match[0];
+                }
+            }
         }
+        //单条返回一维数组，多条返回二维数组
+        return $this->succ(array('sensitive_words' => $matchList));
     }
 }
