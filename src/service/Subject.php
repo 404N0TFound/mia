557e2549d8769a48582c0d679f25d064d19b3e1b
hanxiang <hanxiang@mia.com>
2016-09-22 16:47:40 +0800

    头条bug

--- src/service/Subject.php
+++ src/service/Subject.php
@@ -637,6 +637,7 @@ class Subject extends \mia\miagroup\Lib\Service {
                 $this->subjectModel->viewNumRecord($subjectId, $viewNum);
             }
         }
+        return $subjectCountArr;
     }
     
     /**
