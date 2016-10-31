6126c356c6d75e083f1a907627bfec1e0f50d6f4
zhanghaibo <zhanghaibo@mia.com>
2016-10-31 18:05:25 +0800

    添加蜜豆请求日志

--- src/lib/Thrift.php
+++ src/lib/Thrift.php
@@ -19,11 +19,23 @@ class Thrift {
     public function agent($name,$param){
         $paramJson = json_encode($param);
         try {
+            $request_startTime = gettimeofday(true);
             $data = $this->thriftClient->$name($paramJson);
+            $request_endTime = gettimeofday(true);
+            
+            //记录日志
+            \F_Ice::$ins->mainApp->logger_remote->info(array(
+                'third_server'  =>  'mibean',
+                'request_param' =>  ['func_name'=>$name,'args'=>$param],
+                'response'      =>  $data,
+                'resp_time'     =>  number_format(($request_endTime - $request_startTime), 4),
+            ));
+            //返回结果
             $data = json_decode($data,true);
             return $data;
         } catch (\Exception $e) {
             \F_Ice::$ins->mainApp->logger_remote->warn(array(
+                'third_server'  =>  'mibean',
                 'exception' => get_class($e),
                 'message'   => $e->getMessage(),
                 'code'      => $e->getCode(),
