b0924c3be690c8269ea4e5705a4a9485b23d17c2
xiekun <xiekun@mia.com>
2016-11-29 11:50:04 +0800

    add koubei count

--- src/service/Solr.php
+++ src/service/Solr.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2016/11/29
- * Time: 10:22
- */
+namespace mia\miagroup\Service;
+
+
+class Solr extends \mia\miagroup\Lib\Service {
+
+    public $solr;
+    public $options;
+    public $solrConfig;
+
+    public function __construct() {
+        $this->solrConfig = \F_Ice::$ins->workApp->config->get('solr.solr');
+        $this->solr = new \SolrClient($this->solrConfig['test']);
+    }
+
+    public function koubeiList(){
+
+    }
+
+}
