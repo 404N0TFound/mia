9ec14fa82c7943740a7e40f945986f022b6c9d5d
machaoqun <machaoqun@mia.com>
2017-02-17 11:30:15 +0800

    商品搜索

--- src/remote/Search.php
+++ src/remote/Search.php
@@ -1,11 +1,13 @@
 <?php
 namespace mia\miagroup\Remote;
+use mia\miagroup\Util\NormalUtil;
 
 class Search
 {
     public function __construct()
     {
         //获取配置信息
+        $this->config = \F_Ice::$ins->workApp->config->get('thrift.address.itemSearch');
     }
 
     /**
@@ -30,5 +32,18 @@ class Search
     public function userSearch($keyWords, $page = 1, $count = 20)
     {
         return ["220103494", "1508587", "7509605", "7509576", "7509596", "7509608", "7509603", "7509614", "7509571", "7509569"];
+    }
+
+    /**
+     * 商品搜索
+     * @param $searchArr
+     * @return array
+     */
+    public function itemSearch($searchArr)
+    {
+        $queryStr = http_build_query($searchArr);
+        $url = $this->config['remote'] . $queryStr;
+        $ret = NormalUtil::getCurl('itemSearch', $url, 10);
+        return $ret;
     }
 }
