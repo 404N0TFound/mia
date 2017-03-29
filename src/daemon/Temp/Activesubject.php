ebae235ff63497f3f7e294f443602bf5feae88ec
jiadonghui <jiadonghui@miyabaobei.com>
2017-03-29 11:36:07 +0800

    进行中活动的帖子导入活动帖子关联表脚本

--- src/daemon/Temp/Activesubject.php
+++ src/daemon/Temp/Activesubject.php
@@ -1,10 +1,15 @@
 <?php 
 namespace mia\miagroup\Daemon\Temp;
 
+use mia\miagroup\Service\Active as ActiveService;
+use mia\miagroup\Service\Subject as SubjectService;
+use mia\miagroup\Data\Active\ActiveSubjectRelation as RelationData;
+
 class ActiveSubject extends \FD_Daemon {
     
     public function execute() {
-        $this->fixImgData();
+        $this->setActiveSubjects();
+        //$this->fixImgData();
     }
     
     public function fixImgData() {
@@ -28,5 +33,33 @@ class ActiveSubject extends \FD_Daemon {
             $activeData->updateActive($set_data, $id);
             exit;
         }
+    }
+
+    //将活动的帖子导入活动帖子关联表
+    public function setActiveSubjects(){
+        $activeService = new ActiveService();
+        $subjectService = new SubjectService();
+        $relationData = new RelationData();
+        //获取所有在线活动
+        $activeArrs = $activeService->getCurrentActive();
+        if(!empty($activeArrs['data'])){
+            foreach($activeArrs['data'] as $key=>$activeArr){
+                //获取活动下的帖子
+                $subjectArrs = $subjectService->getActiveSubjects($key, $type='all', false, 0)['data']['subject_lists'];
+                if(empty($subjectArrs)){
+                    continue;
+                }
+                foreach($subjectArrs as $subjectArr){
+                    //将帖子信息存入活动帖子关联表中
+                    $setData = array();
+                    $setData['active_id'] = $activeArr['id'];
+                    $setData['subject_id'] = $subjectArr['id'];
+                    $setData['user_id'] = $subjectArr['user_id'];
+                    $setData['create_time'] = date('Y-m-d H:i:s');
+                    $relationData->addActiveSubjectRelation($setData);
+                }
+            }
+        }
+        return true;
     }
 }
