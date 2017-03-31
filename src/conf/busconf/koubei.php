4481fd32b3664401c51fd8d2f14d04cc3e522cb0
xiekun <xiekun@mia.com>
2017-03-31 17:04:55 +0800

    add config

--- src/conf/busconf/koubei.php
+++ src/conf/busconf/koubei.php
@@ -26,4 +26,76 @@ $itemKoubeiTransfer = array(
     '1688408' => '1021382',
     '1688407' => '1132948',
     '1688406' => '1021380',
-); 
+);
+
+/**
+ * 甄选商品口碑印象标签
+ */
+$selection_quality_labels_A = array(
+    'dimension' => '品质',
+    'tag_info' => array(
+        array(
+            'tag_name' => '很好吃',
+            'positive' => 1,
+        ),
+        array(
+            'tag_name' => '超难吃',
+            'positive' => 2,
+        ),
+    ),
+);
+
+$selection_quality_labels_B = array(
+    'dimension' => '品质',
+    'tag_info' => array(
+        array(
+            'tag_name' => '面料好',
+            'positive' => 1,
+        ),
+        array(
+            'tag_name' => '超级差',
+            'positive' => 2,
+        ),
+    ),
+);
+
+/**
+ * 甄选商品口碑印象标签
+ */
+$selection_price_labels = array(
+    'dimension' => '价格',
+    'tag_info' => array(
+        array(
+            'tag_name' => '物超所值',
+            'positive' => 1,
+        ),
+        array(
+            'tag_name' => '性价比低',
+            'positive' => 2,
+        ),
+    ),
+);
+
+/**
+ * 甄选商品口碑印象标签
+ */
+$selection_exper_labels = array(
+    'dimension' => '体验',
+    'tag_info' => array(
+        array(
+            'tag_name' => '服务好',
+            'positive' => 1,
+        ),
+        array(
+            'tag_name' => '售后差',
+            'positive' => 2,
+        ),
+    ),
+);
+
+/**
+ * 甄选商品分类标签对应关系
+ */
+$selection_cate = array(
+    352 => 'A',
+);
