fd37f2a3c7b48ec700106b8e51b22659b3e0a653
xiekun <xiekun@mia.com>
2017-04-18 17:16:12 +0800

    edit beauty image

---
+++ src/daemon/Subject/Imagebeauty.php
@@ -0,0 +1,18 @@
+<?php
+namespace mia\miagroup\Daemon\Subject;
+
+/**
+ * 美化图片
+ */
+class Imagehandle extends \FD_Daemon {
+
+    private $mode;
+    private $remote;
+    private $tempFilePath;
+
+    public function __construct() {
+
+    }
+
+
+}
