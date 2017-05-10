e795730f00cfd4398b4372bdaa10475b261d5abe
xiekun <xiekun@mia.com>
2017-05-10 16:12:09 +0800

    add image beauty

--- src/daemon/Subject/Imagebeauty.php
+++ src/daemon/Subject/Imagebeauty.php
@@ -11,10 +11,13 @@ class Imagebeauty extends \FD_Daemon
 {
     private $lastIdFile;
     private $tempFilePath;
+    private $image_service;
+    private $subjectData;
 
     public function __construct()
     {
         $this->subjectData = new SubjectData();
+        $this->image_service = new ImageService();
         //加载定时脚本临时文件存放地址
         $runFilePath = \F_Ice::$ins->workApp->config->get('app.run_path');
         $this->tempFilePath = $runFilePath . '/subject/';
@@ -32,15 +35,11 @@ class Imagebeauty extends \FD_Daemon
     public function fullImportSubjectImage()
     {
         ini_set('gd.jpeg_ignore_warning', 1);
-        $image_service = new ImageService();
-        $succFile = '/home/xiekun/succ_img_id';
-        $fp = fopen($succFile, 'a+');
         $this->lastIdFile = $this->tempFilePath . 'beauty_dump_last_id';
         //$this->lastIdFile = 'd:/tmpfile/beauty_dump_last_id';
-        $subjectData = new \mia\miagroup\Data\Subject\Subject();
         //读取上一次处理的id
         if (!file_exists($this->lastIdFile)) { //打开文件
-            $data = $subjectData->query('SELECT max(id) as id FROM group_subjects WHERE ext_info != "" AND image_url != "" AND status = 1');
+            $data = $this->subjectData->query('SELECT max(id) as id FROM group_subjects WHERE ext_info != "" AND image_url != "" AND status = 1 limit 1');
             $lastId = $data[0]['id'] +1;
             $fpLastIdFile = fopen($this->lastIdFile, 'w');
         } else {
@@ -54,51 +53,8 @@ class Imagebeauty extends \FD_Daemon
             $lastId .= fread($fpLastIdFile, 1024);
             $lastId = intval($lastId);
         }
-        $data = $subjectData->query('SELECT id,ext_info FROM group_subjects WHERE ext_info != "" AND status = 1 AND image_url != "" AND id < ' . $lastId . ' LIMIT 100');
-        if (!empty($data)) {
-            foreach ($data as $value) {
-                $subject_id = $value['id'];
-                $beauty = [];
-                $beauty_image = [];
-                // 日志记录
-                fseek($fpLastIdFile, 0, SEEK_SET);
-                ftruncate($fpLastIdFile, 0);
-                fwrite($fpLastIdFile, $subject_id);
-
-                if (!empty($value['ext_info'])) {
-                    $ext_info = json_decode($value['ext_info'], true);
-                    if (!empty($ext_info['beauty_image'])) {
-                        continue;
-                    }
-                    $image_list = $ext_info['image'];
-                    if (!empty($image_list) || count($image_list) > 0) {
-                        foreach ($image_list as $image) {
-                            if (!empty($image['url'])) {
-                                $image_url = $image_service->beautyImage($image['url'])['data'];
-                                if (!empty($image_url)) {
-                                    $beauty['url'] = $image_url;
-                                    if (!empty($image['width'])) {
-                                        $beauty['width'] = $image['width'];
-                                    }
-                                    if (!empty($image['height'])) {
-                                        $beauty['height'] = $image['height'];
-                                    }
-                                }
-                            }
-                            $beauty_image[] = $beauty;
-                        }
-                    }
-                }
-                if (!empty($beauty_image)) {
-                    $ext_info['beauty_image'] = $beauty_image;
-                    $res = $subjectData->query('UPDATE group_subjects SET ext_info = \'' . json_encode($ext_info) . '\' WHERE id = ' . $subject_id);
-                    if (!empty($res)) {
-                        fwrite($fp, $subject_id . "\n");
-                        echo 'success id:' . $subject_id . "\r\n";
-                    }
-                }
-            }
-        }
+        $data = $this->subjectData->query('SELECT id,ext_info FROM group_subjects WHERE ext_info != "" AND status = 1 AND image_url != "" AND id < ' . $lastId . ' order by id desc LIMIT 100');
+        $res = $this->handle_image($data, $fpLastIdFile);
         flock($fpLastIdFile, LOCK_UN);
         fclose($fpLastIdFile);
     }
@@ -109,48 +65,11 @@ class Imagebeauty extends \FD_Daemon
     public function deltaImportSubjectImage()
     {
         ini_set('gd.jpeg_ignore_warning', 1);
-        $image_service = new ImageService();
-        $subjectData = new \mia\miagroup\Data\Subject\Subject();
         $date = date("Y-m-d",time()-86400);
         $startTime = $date . " 00:00:00";
         $endTime = $date . " 23:59:59";
-        $data = $subjectData->query('SELECT id,ext_info FROM group_subjects WHERE ext_info != "" AND image_url != "" AND status = 1 AND created >= "' . $startTime . '" AND created <= "'.$endTime. '"');
-        if (!empty($data)) {
-            foreach ($data as $value) {
-                $subject_id = $value['id'];
-                $beauty = [];
-                $beauty_image = [];
-
-                if (!empty($value['ext_info'])) {
-                    $ext_info = json_decode($value['ext_info'], true);
-                    if (!empty($ext_info['beauty_image'])) {
-                        continue;
-                    }
-                    $image_list = $ext_info['image'];
-                    if (!empty($image_list) && count($image_list) > 0) {
-                        foreach ($image_list as $image) {
-                            if (!empty($image['url'])) {
-                                $image_url = $image_service->beautyImage($image['url'])['data'];
-                                if (!empty($image_url)) {
-                                    $beauty['url'] = $image_url;
-                                    if (!empty($image['width'])) {
-                                        $beauty['width'] = $image['width'];
-                                    }
-                                    if (!empty($image['height'])) {
-                                        $beauty['height'] = $image['height'];
-                                    }
-                                }
-                            }
-                            $beauty_image[] = $beauty;
-                        }
-                    }
-                }
-                if (!empty($beauty_image)) {
-                    $ext_info['beauty_image'] = $beauty_image;
-                    $subjectData->query('UPDATE group_subjects SET ext_info = \'' . json_encode($ext_info) . '\' WHERE id = ' . $subject_id);
-                }
-            }
-        }
+        $data = $this->subjectData->query('SELECT id,ext_info FROM group_subjects WHERE ext_info != "" AND image_url != "" AND status = 1 AND created >= "' . $startTime . '" AND created <= "'.$endTime. '"');
+        $this->handle_image($data);
     }
 
 
@@ -233,5 +152,56 @@ class Imagebeauty extends \FD_Daemon
                 fclose($fpLastIdFile);
             }
         }
+    }
+
+    public function handle_image($data = array(), $fpLastIdFile)
+    {
+        $res = 0;
+        if (!empty($data)) {
+            foreach ($data as $value) {
+                $subject_id = $value['id'];
+                $beauty = [];
+                $beauty_image = [];
+                // 日志记录
+                if(!empty($fpLastIdFile)) {
+                    fseek($fpLastIdFile, 0, SEEK_SET);
+                    ftruncate($fpLastIdFile, 0);
+                    fwrite($fpLastIdFile, $subject_id);
+                }
+                if (empty($value['ext_info'])) {
+                    return $res;
+                }
+                $ext_info = json_decode($value['ext_info'], true);
+                    if (!empty($ext_info['beauty_image'])) {
+                        echo 6;
+                        continue;
+                    }
+                    $image_list = $ext_info['image'];
+                    if(empty($image_list)) {
+                        return $res;
+                    }
+                    echo 7;
+                    foreach ($image_list as $image) {
+                        if (!empty($image['url'])) {
+                            $image_url = $this->image_service->beautyImage($image['url'])['data'];
+                            if (!empty($image_url)) {
+                                $beauty['url'] = $image_url;
+                                if (!empty($image['width'])) {
+                                    $beauty['width'] = $image['width'];
+                                }
+                                if (!empty($image['height'])) {
+                                    $beauty['height'] = $image['height'];
+                                }
+                            }
+                        }
+                        $beauty_image[] = $beauty;
+                    }
+                }
+                if (!empty($beauty_image)) {
+                    $ext_info['beauty_image'] = $beauty_image;
+                    $res = $this->subjectData->query('UPDATE group_subjects SET ext_info = \'' . json_encode($ext_info) . '\' WHERE id = ' . $subject_id);
+                }
+            }
+        return $res;
     }
 }
