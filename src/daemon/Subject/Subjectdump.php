4294138f2e41c9632e5b9b33a2d00f977b04a993
hanxiang <hanxiang@mia.com>
2017-08-04 15:27:39 +0800

    导出文件首页长文标识

--- src/daemon/Subject/Subjectdump.php
+++ src/daemon/Subject/Subjectdump.php
@@ -301,6 +301,9 @@ class Subjectdump extends \FD_Daemon {
             }
             //帖子类型
             $dumpdata['subject_type'] = $subject['type'];
+            if ($subject['type'] == 'blog' && !empty($extInfo['index_cover'])) {
+                $dumpdata['subject_type'] = 'index_blog';
+            }
             //写入文本
             $put_content = implode("\t", $dumpdata);
             file_put_contents($this->dumpSubjectFile, $put_content . "\n", FILE_APPEND);
