5face700949d1a2daa2f91be173402f0e5586ff5
jiadonghui <jiadonghui@miyabaobei.com>
2017-08-15 16:43:32 +0800

    服务号发现页素材列表修改

--- src/service/Feed.php
+++ src/service/Feed.php
@@ -136,7 +136,7 @@ class Feed extends \mia\miagroup\Lib\Service {
         }
         
         //获取帖子详细信息
-        $subjectsList = $this->subjectService->getBatchSubjectInfos($subjectIds,$currentUid,array('item'))['data'];
+        $subjectsList = $this->subjectService->getBatchSubjectInfos($subjectIds,$currentUid,array('user_info', 'count', 'content_format', 'album','item'))['data'];
         $data = [];
         $data['subject_lists'] = array_values($subjectsList);
         return $this->succ($data);
