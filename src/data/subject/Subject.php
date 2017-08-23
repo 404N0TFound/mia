a37cd2d56040efb64281dca06975ec6554d4561b
hanxiang <hanxiang@mia.com>
2017-08-23 17:31:51 +0800

    长文审核相关

--- src/data/subject/Subject.php
+++ src/data/subject/Subject.php
@@ -166,18 +166,6 @@ class Subject extends \DB_Query {
     }
     
     /**
-     * 删除帖子
-     */
-    public function delete($subjectId,$userId){
-        //删除帖子
-        $setData[] = ['status', \F_Ice::$ins->workApp->config->get('busconf.subject.status.user_delete')];
-        $where[] = ['id',$subjectId];
-        $where[] = ['user_id',$userId];
-        $affect = $this->update($setData,$where);
-        return $affect;
-    }
-    
-    /**
      * 精选帖子的ids
      * @param int $iPage 页码
      * @param int $iPageSize 一页多少个
