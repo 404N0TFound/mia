01d08bf757455f0a752040afbee35b113824e6ae
hanxiang <hanxiang@mia.com>
2017-09-05 14:36:22 +0800

    消消乐活动支持

--- src/model/ums/Active.php
+++ src/model/ums/Active.php
@@ -9,7 +9,7 @@ class Active extends \DB_Query {
 
     protected $tableActive = 'group_active';
     protected $tableActiveSubjectRelation = 'group_subject_active_relation';
-
+    protected $tableSubjectPointTags = 'group_subject_point_tags';
 
     public function getGroupActiveData($month)
     {
@@ -54,6 +54,44 @@ class Active extends \DB_Query {
                 $result['list'][$k] = $v;
             }
         }
+        return $result;
+    }
+    
+    /**
+     * 根据商品id获取参加活动的帖子
+     */
+    public function getActiveSubjectByItem($cond, $offset = 0, $limit = 50, $orderBy = 'id desc') {
+        $this->tableName = $this->tableActiveSubjectRelation;
+        $orderBy = $this->tableName. '.id desc';
+        $result = array('count' => 0, 'list' => array());
+        $where = array();
+        if (!empty($cond)) {
+            //组装where条件
+            foreach ($cond as $k => $v) {
+                switch ($k) {
+                    case 'start_time':
+                        $where[] = [':ge','create_time', $v];
+                        break;
+                    case 'end_time':
+                        $where[] = [':le','create_time', $v];
+                        break;
+                    case 'item_id':
+                        $where[] = ['pt.item_id', $v];
+                    default:
+                        $where[] = [$k, $v];
+                }
+            }
+        }
+        
+        $join = 'left join '.$this->tableSubjectPointTags. ' as pt on ' .$this->tableName . '.subject_id=pt.subject_id ';
+        $field = 'distinct '. $this->tableName. '.subject_id';
+        $result['count'] = $this->count($where, $join, $field);
+        
+        if (intval($result['count']) <= 0) {
+            return $result;
+        }
+
+        $result['list'] = $this->getRows($where, $field, $limit, $offset, $orderBy, $join);
         return $result;
     }
 }
